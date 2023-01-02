92164378f91d186628766c4d81abca777c400df3
Dominick Meglio <dmeglio@gmail.com>
2023-01-01 20:14:27 -0500

    Fixed a bad library requirement from some previous testing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="petsafe",
-    version="2.0",
+    version="2.0.1",
     author="Jordan Stremming & Dominick Meglio",
     license="MIT",
     author_email="dmeglio@gmail.com",
@@ -14,7 +14,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/dcmeglio/petsafe",
     packages=setuptools.find_packages(),
-    install_requires=["httpx", "aioboto3"],
+    install_requires=["httpx", "aiobotocore"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
