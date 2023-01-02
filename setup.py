e5bbd55b02bc40246243326b08695aae45aed27a
Dominick Meglio <dmeglio@gmail.com>
2023-01-01 22:06:54 -0500

    Increased version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="petsafe",
-    version="2.0.1",
+    version="2.0.2",
     author="Jordan Stremming & Dominick Meglio",
     license="MIT",
     author_email="dmeglio@gmail.com",
